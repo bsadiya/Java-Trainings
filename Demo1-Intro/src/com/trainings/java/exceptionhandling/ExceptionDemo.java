ec5c908d483523799634b9ec3feac4be15c2b789
Sadiya <sadbegum@127.0.0.1>
2020-11-26 20:01:18 +0530

    Exception

---
+++ Demo1-Intro/src/com/trainings/java/exceptionhandling/ExceptionDemo.java
@@ -0,0 +1,37 @@
+package com.trainings.java.exceptionhandling;
+
+public class ExceptionDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Arthmt operation Start!!!!!");
+		Test.arithMeticOperation();
+		System.out.println("Arthmt operation performed!!!!!");
+
+	}
+//	stack --> main()
+//	stack --> main()->arithMeticOperation() 
+//	stack --> main()->arithMeticOperation() ->divide()
+//	stack --> main()->arithMeticOperation() ->divide() -> divideZero()
+	
+//	stack --> main()->arithMeticOperation() ->divide() -> divideZero()
+
+}
+
+class Test {
+	public static void arithMeticOperation() {
+		divide();
+	}
+
+	public static void divide() {
+		divideZero();
+	}
+
+	public static void divideZero() {
+		
+		System.out.println("Dividing with Zero Satrt!!!!");
+		
+		System.out.println(10 / 0);
+		
+		System.out.println("Divided by Zero Succesfully.....");
+	}
+}
